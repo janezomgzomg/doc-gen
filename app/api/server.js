9c62d7f104f274b3475ba19f41d9d379bd1219f2
Jane Mendonca <jane.zomg@gmail.com>
2015-04-11 16:41:44 +0400

    -improved structure of the app -page loads with node server running -separated css from the html file

--- app/api/server.js
+++ app/api/server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
-app.use(express.static('../ui'));
+app.use(express.static('../ui/base/'));
 
 var server = app.listen(1337, function(){
   var host = server.address().address;
   var port = server.address().port;
   console.log('Server is running at http://%s:%s', host, port);
+});
+
+var urls = {
+  '/' : '/site'
+};
+app.get('/', function(request, response){
+  response.writeHead(301,
+  {Location: urls['/']});
+  response.end();
 });
